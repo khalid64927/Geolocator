797878e4c4f8167b7853ae46fd104507592119b0
Mohammed Khalid Hamid <khalid64927@gmail.com>
2023-05-22 14:18:17 +0800

    WIP added appsflyer branch

--- app/src/main/java/com/sprotte/geolocator/demo/App.kt
+++ app/src/main/java/com/sprotte/geolocator/demo/App.kt
@@ -2,12 +2,13 @@ package com.sprotte.geolocator.demo
 
 import android.app.Application
 import timber.log.Timber
+import com.appsflyer.AppsFlyerLib
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        AppsFlyerLib.getInstance().init("6VTPGqbLbdMPSBkiiTzEMJ", null, this)
     }
 }
